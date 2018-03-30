b54c300dee93eacee6d5dc1bd65e282aa3cb689b
Xiyng <xiyng.dev@gmail.com>
2018-03-31 01:51:02 +0300

    Add a test case for invalid RLEW tags

--- src/gamemaps_parser/header/tests.rs
+++ src/gamemaps_parser/header/tests.rs
@@ -7,3 +7,11 @@ fn does_not_parse_empty_data() {
         Err(HeaderParseError::UnexpectedEndOfFile)
     );
 }
+
+#[test]
+fn does_not_parse_invalid_rlew_tag() {
+    assert_eq!(
+        parse(&vec![0xfe, 0xef]),
+        Err(HeaderParseError::InvalidRlewTag(0xeffe))
+    );
+}
