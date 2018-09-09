c93d8763b1cfc5d09017cfcd2e95a8a874fd7be6
Xiyng <xiyng.dev@gmail.com>
2018-09-09 22:04:49 +0300

    Implement Carmack decompression for near pointers

---
+++ src/gamemaps_parser/compression/carmack/mod.rs
@@ -0,0 +1,54 @@
+#[cfg(test)]
+mod tests;
+
+extern crate byteorder;
+
+// use std::fmt;
+use self::byteorder::*;
+
+pub fn decompress(data: &Vec<u8>) -> Result<Vec<u16>, DecompressionError> {
+    let mut decompressed = Vec::new();
+
+    let mut i  = 0;
+    while i < data.len() {
+        if i + 1 >= data.len() {
+            return Err(DecompressionError::InvalidLength(data.len()));
+        }
+
+        let current = data[i];
+        let next = data[i + 1];
+        i += 2;
+        match next {
+            0xa7 => {
+                if i >= data.len() {
+                    return Err(DecompressionError::InvalidLength(data.len()));
+                }
+
+                let repeat_offset = 2 * data[i]; // as words, not bytes
+                let repeat_start = i - repeat_offset as usize; // TODO: Check for range.
+                let mut words = vec![0; current as usize];
+                LittleEndian::read_u16_into(
+                    &data[repeat_start..repeat_start + (2 * current) as usize],
+                    &mut words
+                );
+                decompressed.append(&mut words);
+                i += 1;
+            },
+            0xa8 => {
+                if i + 1 >= data.len() {
+                    return Err(DecompressionError::InvalidLength(data.len()));
+                }
+
+                unimplemented!()
+            },
+            _ => decompressed.push(LittleEndian::read_u16(&vec![current, next]))
+        }
+    }
+
+    Ok(decompressed)
+}
+
+#[derive(Debug, PartialEq)]
+pub enum DecompressionError {
+    InvalidLength(usize)
+}
---
+++ src/gamemaps_parser/compression/carmack/tests.rs
@@ -0,0 +1,38 @@
+use super::*;
+
+extern crate byteorder;
+
+struct SuccessTestData {
+    compressed: Vec<u8>,
+    decompressed: Vec<u16>
+}
+
+fn assert_success(test_data: SuccessTestData) {
+    let actual = decompress(&test_data.compressed);
+    let expected = Ok(test_data.decompressed);
+    assert_eq!(actual, expected)
+}
+
+#[test]
+fn works_with_empty_data() {
+    assert_success(SuccessTestData {
+        compressed: vec![],
+        decompressed: vec![]
+    })
+}
+
+#[test]
+fn does_not_modify_uncompressed_data() {
+    assert_success(SuccessTestData {
+        compressed: vec![0xcd, 0x00],
+        decompressed: vec![0x00cd]
+    })
+}
+
+#[test]
+fn decompresses_with_one_near_pointer() {
+    assert_success(SuccessTestData {
+        compressed: vec![0xcd, 0x00, 0x01, 0xa7, 0x02],
+        decompressed: vec![0x00cd, 0x00cd]
+    })
+}
