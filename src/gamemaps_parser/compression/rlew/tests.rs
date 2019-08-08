6aa3ec2620aeb44088428be11ca79fd5ee05dc6d
Xiyng <xiyng.dev@gmail.com>
2019-08-09 00:15:48 +0300

    Add a test with real data

--- src/gamemaps_parser/compression/rlew/tests.rs
+++ src/gamemaps_parser/compression/rlew/tests.rs
@@ -5,6 +5,7 @@ extern crate byteorder;
 use self::byteorder::{LittleEndian, WriteBytesExt};
 
 const RLEW_TAG: u16 = 0xfefe;
+const WOLF3D_RLEW_TAG: u16 = 0xabcd;
 
 struct SuccessTestData {
     compressed: Vec<u16>,
@@ -38,4 +39,12 @@ fn decodes_two_repeated_values() {
         compressed: vec![0x0a, 0x00, 0x00, 0x00, RLEW_TAG, 2, 0xabcd, RLEW_TAG, 3, 0xbcde],
         decompressed: vec![0xabcd, 0xabcd, 0xbcde, 0xbcde, 0xbcde]
     })
+}
+
+#[test]
+fn decodes_wolf1_map1_plane2() {
+    let compressed_data = vec![0x00, 0x20, 0xcd, 0xab, 0x00, 0x10, 0x00, 0x00];
+    let decompressed_data = decode(&compressed_data, WOLF3D_RLEW_TAG).unwrap();
+    assert_ne!(decompressed_data.len(), 0); // This is not really needed but it can clean up the failure message.
+    assert_eq!(decompressed_data, vec![0x0000; 0x1000])
 }
